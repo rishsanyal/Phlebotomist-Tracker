3d8ffa26e63a5f70ca66edf09701a484b3051388
rishsanyal <rishabh106@gmail.com>
2025-04-21 19:39:06 -0700

    adding clinician info

---
+++ clinicians.py
@@ -0,0 +1,55 @@
+from enum import Enum
+
+class ClinicianStatus(Enum):
+    """Enum representing the status of a clinician"""
+    INITIALIZED = 1
+    WITHIN_BOUNDS = 2
+    OUT_OF_BOUNDS = 3
+    ERROR = 4
+
+
+INITIAL_DATA = {
+    1: {
+        "user_id": 1,
+        "query_status": ClinicianStatus.INITIALIZED,
+        "error_count": 0,
+        "error_message_sent": False,
+        # "last_checked": datetime.utcnow()
+    },
+    # 2: {
+    #     "user_id": 2,
+    #     "query_status": ClinicianStatus.INITIALIZED,
+    #     "error_count": 0,
+    #     "error_message_sent": False
+    # },
+    # 3: {
+    #     "user_id": 3,
+    #     "query_status": ClinicianStatus.INITIALIZED,
+    #     "error_count": 0,
+    #     "error_message_sent": False
+    # },
+    # 4: {
+    #     "user_id": 4,
+    #     "query_status": ClinicianStatus.INITIALIZED,
+    #     "error_count": 0,
+    #     "error_message_sent": False
+    # },
+    # 5: {
+    #     "user_id": 5,
+    #     "query_status": ClinicianStatus.INITIALIZED,
+    #     "error_count": 0,
+    #     "error_message_sent": False
+    # },
+    # 6: {
+    #     "user_id": 6,
+    #     "query_status": ClinicianStatus.INITIALIZED,
+    #     "error_count": 0,
+    #     "error_message_sent": False
+    # },
+    # 7: {
+    #     "user_id": 7,
+    #     "query_status": ClinicianStatus.INITIALIZED,
+    #     "error_count": 0,
+    #     "error_message_sent": False
+    # }
+}
